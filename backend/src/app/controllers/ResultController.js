bb9762f03cb87b1e48cefe8fa62e0262741af6e8
Gabriel <knuckles4x@gmail.com>
2020-04-10 23:46:31 -0300

    set points to user by round

--- backend/src/app/controllers/ResultController.js
+++ backend/src/app/controllers/ResultController.js
@@ -1,6 +1,7 @@
 import Round from '../models/Round';
 import Match from '../models/Match';
 import Bet from '../models/Bet';
+import User from '../models/User';
 
 class MatchController {
   async update(req, res) {
@@ -14,24 +15,33 @@ class MatchController {
       ],
     });
 
-    async function setWins() {
-      round.Matches.forEach(async (match) => {
-        await Bet.update(
-          { win: true },
-          {
-            where: {
-              match_id: match.id,
-              choice: match.winner,
-            },
-          }
-        );
+    round.Matches.forEach(async (match) => {
+      await Bet.update(
+        { win: true },
+        {
+          where: {
+            match_id: match.id,
+            choice: match.winner,
+          },
+        }
+      );
+    });
+
+    const users = await User.findAll({ attributes: ['id'] });
+    users.forEach(async (user) => {
+      const points = await Bet.count({
+        where: {
+          user_id: user.dataValues.id,
+          win: true,
+        },
       });
-    }
-
-    async function addPoints() {}
-
-    await setWins();
-    // await addPoints();
+      await User.update(
+        {
+          points,
+        },
+        { where: { id: user.dataValues.id } }
+      );
+    });
 
     return res.json(round.Matches);
   }
--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -10,6 +10,7 @@ class User extends Model {
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         is_admin: Sequelize.BOOLEAN,
+        points: Sequelize.INTEGER,
       },
       {
         sequelize,
