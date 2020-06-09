bdb57fc51e048e1f0a27bd55722a89f0dc77f5f1
Gabriel <knuckles4x@gmail.com>
2020-06-09 00:22:08 -0300

    move points update from ResultController to a new controller (PointsController)

--- backend/src/app/controllers/ResultController.js
+++ backend/src/app/controllers/ResultController.js
@@ -1,13 +1,10 @@
-import Sequelize from 'sequelize';
 import Round from '../models/Round';
 import Match from '../models/Match';
 import Bet from '../models/Bet';
-import User from '../models/User';
 
-class MatchController {
+class ResultController {
   // update points by round
   async update(req, res) {
-    // get round
     const round = await Round.findByPk(req.params.id, {
       attributes: [],
       include: [
@@ -32,35 +29,8 @@ class MatchController {
       })
     );
 
-    const users = await User.findAll({
-      attributes: {
-        include: [
-          [
-            Sequelize.literal(`(
-          SELECT COUNT(*)
-          FROM "bets" AS "Bet"
-          WHERE
-              "Bet"."user_id" = "User"."id"
-              AND
-              "Bet"."win" = true
-      )`),
-            'points',
-          ],
-        ],
-      },
-    });
-
-    await Promise.all(
-      users.map(async (user) => {
-        await User.update(
-          { points: user.dataValues.points },
-          { where: { id: user.dataValues.id } }
-        );
-      })
-    );
-
     return res.json(round.Matches);
   }
 }
 
-export default new MatchController();
+export default new ResultController();
