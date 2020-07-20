6686cb9dffed371420b1f9aa484ba83468fdc0fb
Gabriel <knuckles4x@gmail.com>
2020-07-20 00:36:55 -0300

    add is_caster field to users

---
+++ backend/src/database/migrations/20200720033309-add-iscaster-field-to-user.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'is_caster', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'is_caster');
+  },
+};
