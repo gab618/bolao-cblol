e4ae6f3d06960fcb40b0af27652a23f0fa7a4464
Gabriel <knuckles4x@gmail.com>
2020-07-25 17:29:01 -0300

    nodemailer config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '35e2dc0a5e800d',
+    pass: '9a7b235a9d832a',
+  },
+  default: {
+    from: 'Gabriel | BolãoCBLOL <noreply@bolaocblol.com>',
+  },
+};
