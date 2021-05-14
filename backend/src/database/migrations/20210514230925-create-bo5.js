01d9141641e4b7cb950d35ecf6f3af0e721871ca
Gabriel <knuckles4x@gmail.com>
2021-05-14 20:32:46 -0300

    migration: create bo5_bets

---
+++ backend/src/database/migrations/20210514230925-create-bo5.js
@@ -0,0 +1,58 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('bo5_bets', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      match_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'matches', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false,
+      },
+      blue_team_wins: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      red_team_wins: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      win: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('bo5_bets');
+  },
+};
